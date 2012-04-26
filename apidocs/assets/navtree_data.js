32583022684fe89391bd97dbaaf4ffdc4f5b4772
simonetripodi <simone.tripodi@gmail.com>
2012-04-26 11:23:50 +0200

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.guice.lifegycle", "org/nnsoft/guice/lifegycle/package-summary.html", [ [ "Description", "org/nnsoft/guice/lifegycle/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "DisposeHandler", "org/nnsoft/guice/lifegycle/DisposeHandler.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AfterInjectionModule", "org/nnsoft/guice/lifegycle/AfterInjectionModule.html", null, "" ], [ "DisposeModule", "org/nnsoft/guice/lifegycle/DisposeModule.html", null, "" ], [ "Disposer", "org/nnsoft/guice/lifegycle/Disposer.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "AfterInjection", "org/nnsoft/guice/lifegycle/AfterInjection.html", null, "" ], [ "Dispose", "org/nnsoft/guice/lifegycle/Dispose.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.guice.lifegycle", link:"org/nnsoft/guice/lifegycle/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.lifegycle.AfterInjection", link:"org/nnsoft/guice/lifegycle/AfterInjection.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.lifegycle.AfterInjectionModule", link:"org/nnsoft/guice/lifegycle/AfterInjectionModule.html", type:"class" },
+      { id:3, label:"org.nnsoft.guice.lifegycle.Dispose", link:"org/nnsoft/guice/lifegycle/Dispose.html", type:"class" },
+      { id:4, label:"org.nnsoft.guice.lifegycle.DisposeHandler", link:"org/nnsoft/guice/lifegycle/DisposeHandler.html", type:"class" },
+      { id:5, label:"org.nnsoft.guice.lifegycle.DisposeModule", link:"org/nnsoft/guice/lifegycle/DisposeModule.html", type:"class" },
+      { id:6, label:"org.nnsoft.guice.lifegycle.Disposer", link:"org/nnsoft/guice/lifegycle/Disposer.html", type:"class" }
+
+    ];
